6ae1f735b81d4fa2f491fb40096d5af9fe2e0f0b
arpita <arpitacse09@gmail.com>
2026-03-19 22:45:34 -0400

    Initial Commit

---
+++ SwiftUI_Todo_App/SwiftUI_Todo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Todo_AppApp.swift
+//  SwiftUI_Todo_App
+//
+//  Created by Arpita  Pradhan on 3/19/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Todo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
